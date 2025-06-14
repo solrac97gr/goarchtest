e7d937747ec585379f40cc363fb41dda51ece4b4
solrac97gr <carlosgarcia_97gr@hotmail.com>
2025-06-14 10:41:56 +0200

    docs: Enhance documentation for CheckRule method to clarify parameters and return values

--- goarchtest.go
+++ goarchtest.go
@@ -13,6 +13,17 @@ func New(path string) *GoArchTest {
 }
 
 // CheckRule executes a predefined architectural rule
+// It takes a rule function that operates on the Types and returns a Result.
+// This function is used to apply various architectural checks on the types defined in the Go project.
+// Parameters:
+//   - rule: A function that takes a pointer to Types and returns a Result
+//
+// Returns:
+//   - *Result: Returns the result of applying the rule, which includes whether the rule passed or failed
+//
+// Example:
+//
+//	result := goarchtest.CheckRule(goarchtest.ResideInNamespace("github.com/myorg/mypackage"))
 func (g *GoArchTest) CheckRule(rule func(*Types) *Result) *Result {
 	return rule(g.Types)
 }
